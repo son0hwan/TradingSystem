a85a7ff29f9ca29e8777ae917ca497e9481db18c
sj21.lim <lsj960427@gmail.com>
2025-07-04 15:16:21 +0900

    [feature] sell nice

--- Project28/autoTradingSystem.h
+++ Project28/autoTradingSystem.h
@@ -11,13 +11,20 @@ public:
 	void buy(std::string stockCode, int price, int count);
 	int getPrice(std::string stockCode);
 	int buyNiceTiming(std::string stockCode, int price);
+	int sellNiceTiming(std::string stockCode, int stockCount);
 
 private:
 	bool IsPriceIncrease3Times(std::string& stockCode, int price);
 	void checkBuyPrerequisite(std::string stockCode, int price, int count);
 
+	int getNiceCellPrice(const std::string& stockCode);
+
+	bool isValidStockPrice(int price);
+
 	StockBrockerDriver* driver;
 	static const int MAX_BUY_COUNT = 3;
+	static const int MAX_SELL_COUNT = 3;
+	static const int INVALID_STOCK_PRICE = -1;
 };
 
 class BrockerFinder {
