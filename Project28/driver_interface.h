76f18e36afb9edb042e7e32d4beba4655767eba8
sj21.lim <lsj960427@gmail.com>
2025-07-04 09:49:02 +0900

    Support driver interface

---
+++ Project28/driver_interface.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+class StockBrockerDriver {
+public:
+	virtual void login(std::string ID, std::string password) = 0;
+
+	virtual void buy(std::string stockCode, int price, int count) = 0;
+
+	virtual void sell(std::string stockCode, int price, int count) = 0;
+
+	virtual int getPrice(std::string stockCode) = 0;
+};
--- Project28/test.cpp
+++ Project28/test.cpp
@@ -1,4 +1,13 @@
 #include "gmock/gmock.h"
+#include "driver_interface.h"
+
+class MockDriver : public StockBrockerDriver {
+public:
+	MOCK_METHOD(void, login, (std::string, std::string), (override));
+	MOCK_METHOD(void, buy, (std::string, int, int), (override));
+	MOCK_METHOD(void, sell, (std::string, int, int), (override));
+	MOCK_METHOD(int, getPrice, (std::string), (override));
+};
 
 TEST(TS, TC1) {
 	EXPECT_EQ(1, 1);
