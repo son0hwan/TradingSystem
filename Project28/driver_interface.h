cc5f50b3c68ac91ae04fd451aa57a45caadf7cf9
sj21.lim <lsj960427@gmail.com>
2025-07-04 09:49:02 +0900

    add api

---
+++ Project28/driver_interface.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+class TradingDriverAPI {
+public:
+	virtual void login(std::string ID, std::string password) = 0;
+
+	virtual void buy(std::string stockCode, int count, int price) = 0;
+
+	virtual void sell(std::string stockCode, int count, int price) = 0;
+
+	virtual int getPrice(std::string stockCode) = 0;
+};
