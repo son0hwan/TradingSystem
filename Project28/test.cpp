967b96a22b40d6cdfcffcd7c6701a8d18ec0f08c
sonny <sonny@google.com>
2025-07-04 10:40:20 +0900

    [App] login test

--- Project28/test.cpp
+++ Project28/test.cpp
@@ -1,5 +1,7 @@
 #include "gmock/gmock.h"
 #include "driver_interface.h"
+#include <iostream>
+
 
 TEST(Application, selectKiwerStockBrocker) {
 	Application app;
@@ -12,4 +14,26 @@ TEST(Application, selectNemoStockBrocker) {
 	StockBrockerDriver* brocker = app.selectStockBrocker("nemo");
 	
 	EXPECT_TRUE(brocker != nullptr);
+}
+
+TEST(Application, logInKiwerSTockBrocker) {
+	Application app;
+	std::string id = "adminId";
+	std::string pwd = "adminPwd";
+	StockBrockerDriver* brocker = app.selectStockBrocker("kiwer");
+	
+	bool loginSuccess = app.login(id, pwd);
+
+	EXPECT_TRUE(loginSuccess);
+}
+
+TEST(Application, logInNemoSTockBrocker) {
+	Application app;
+	std::string id = "adminId";
+	std::string pwd = "adminPwd";
+	StockBrockerDriver* brocker = app.selectStockBrocker("nemo");
+	
+	bool loginSuccess = app.login(id, pwd);
+
+	EXPECT_TRUE(loginSuccess);
 }
